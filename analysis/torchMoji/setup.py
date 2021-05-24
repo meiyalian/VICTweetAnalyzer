e6ef0a7f54827cbaf87bf2bc3638625d4a990eac
JIAYING WANG <jwan0085@student.monash.edu>
2021-05-24 18:33:00 +1000

    install package

--- analysis/torchMoji/setup.py
+++ analysis/torchMoji/setup.py
@@ -5,12 +5,12 @@ setup(
     version='1.0',
     packages=['torchmoji'],
     description='torchMoji',
-    include_package_data=True,
-    install_requires=[
-        'emoji==0.4.5',
-        # 'numpy==1.13.1',
-        'scipy==0.19.1',
-        'scikit-learn==0.19.0',
-        'text-unidecode==1.0',
-    ],
+    include_package_data=True
+    # install_requires=[
+    #     'emoji==0.4.5',
+    #     # 'numpy==1.13.1',
+    #     'scipy==0.19.1',
+    #     'scikit-learn==0.19.0',
+    #     'text-unidecode==1.0',
+    # ],
 )
